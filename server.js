11418fef0716f689d83412a1a746102000103ab7
jonathanrlin <jonathanrlin@gmail.com>
2019-11-20 15:33:50 -0800

    Chg: Setup MySQL with sequelize ORM. (#5)
    With User table as an example.

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const path = require("path");
 const express = require("express");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./server/models/user");
 
 const app = express();
 
@@ -10,21 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public/dist")));
 
-//passport config
-app.use(
-  require("express-session")({
-    secret: "secret code goes here",
-    resave: false,
-    saveUninitialized: false
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-require("./server/config/mongoose");
 require("./server/config/routes")(app);
 app.all("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/dist/index.html"));
--- server/config/routes.js
+++ server/config/routes.js
@@ -2,5 +2,5 @@ const usersController = require("../controllers/users");
 
 module.exports = function(app) {
   app.get("/api/users", usersController.all);
-  app.post("/api/users", usersController.create);
+  app.post("/api/user/new", usersController.create);
 };
