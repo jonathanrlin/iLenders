536803e70740eaacd98192d6e7827536affe4933
Jonathan Lin <jonathanrlin@gmail.com>
2019-11-21 11:56:21 -0800

    Add: Details page as well as the new lend page. Add: A red marker on the details page as well as creating a lending and selling create pages under icons and typography folders as well as got rid of the side settings cog.

--- public/src/app/components/sidebar/sidebar.component.ts
+++ public/src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 declare const $: any;
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Browse',  icon: 'dashboard', class: '' },
@@ -14,27 +14,27 @@ export const ROUTES: RouteInfo[] = [
     { path: '/table-list', title: 'Order History',  icon:'content_paste', class: '' },
     { path: '/typography', title: 'Lend',  icon:'library_books', class: '' },
     { path: '/icons', title: 'Sell',  icon:'bubble_chart', class: '' },
-    // { path: '/maps', title: 'Details',  icon:'location_on', class: '' },
+    { path: '/maps', title: 'Details',  icon:'location_on', class: '' },
     // { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
 ];
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"]
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
   isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
-      }
-      return true;
-  };
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
+  }
 }
