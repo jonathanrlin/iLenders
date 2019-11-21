29a95f45cd70081f33d3ee18e977bca8984f2b1d
Jonathan Lin <jonathanrlin@gmail.com>
2019-11-20 17:28:28 -0800

    Add: Basic CRUD for user. Get all, create one, update by id, delete by id.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const usersController = require("../controllers/users");
+const usersController = require('../controllers/users');
 
-router.get("/all", usersController.all);
-router.post("/new", usersController.create);
+router.get('/all', usersController.all);
+router.post('/new', usersController.create);
+router.put('/update', usersController.updateById);
+router.delete('/del', usersController.deleteById);
 
 module.exports = router; 
